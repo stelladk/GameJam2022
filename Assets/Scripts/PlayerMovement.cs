02f8af3182b02204380a9f8e99889acec1411782
Stella Douka <35050451+stelladk@users.noreply.github.com>
2022-05-22 19:33:49 +0300

    CharacterController

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController2D controller;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
